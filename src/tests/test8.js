f2018874d809e1c50cf8e6c401d6f474491b44ca
Christopher Little <hephistocles@gmail.com>
2014-12-18 23:05:26 +0000

    Add test for scope in ifs and first 'real' function

---
+++ src/tests/test8.js
@@ -0,0 +1,10 @@
+// jstyper start
+	
+if (true) {
+	// try initialising the scopeTest var outside the if
+	scopeTest = true;
+} else {
+	scopeTest = 5;
+}
+
+// jstyper end
---
+++ src/tests/test9.js
@@ -0,0 +1,14 @@
+function yesIfBiggerThanFive(z) {
+	
+	// jstyper start import z
+	var five = 5, test = z - five > 0, returnVal;
+	
+	if (test) {
+		returnVal = 'yes';
+	} else {
+		returnVal = 'no';		
+	}
+
+	// jstyper end
+	return returnVal;
+}
