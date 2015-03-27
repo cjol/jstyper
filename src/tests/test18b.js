0ce23ad917df6085a58f74bb6f114ae7a39d8d72
Christopher Little <hephistocles@gmail.com>
2015-03-27 19:29:17 +0000

    Here are the actual test results...

---
+++ src/tests/test18b.js
@@ -0,0 +1,17 @@
+// expected result: success
+
+// jstyper start
+function rectangleArea() {
+	return this.height * this.width;
+}
+function circleArea() {
+	return this.radius * this.radius;
+}
+
+var circle = {radius:3, area: circleArea};
+
+var rectangle = {height:2, width:3, area:rectangleArea};
+
+var totalArea = circle.area() + rectangle.area();
+
+// jstyper end
