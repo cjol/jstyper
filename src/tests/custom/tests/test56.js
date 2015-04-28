ada9d0a9fd3ea2ebe8b34c2b183abcd3f9a7f96a
Christopher Little <hephistocles@gmail.com>
2015-04-28 22:41:27 +0100

    More tests

---
+++ src/tests/custom/tests/test56.js
@@ -0,0 +1,7 @@
+// jstyper start
+
+var x = [1,2,3];
+var len = x.length;
+len += 2;
+
+// jstyper end
---
+++ src/tests/custom/tests/test57.js
@@ -0,0 +1,9 @@
+// jstyper start
+
+function sceneIntersect(xs) {
+	var x = xs[0];
+	x.foo ++;
+	x.bar++;
+}
+
+// jstyper end
