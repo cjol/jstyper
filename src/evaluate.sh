4340a7ace1c85ecfe569f8d338afdc49cdba3625
Christopher Little <hephistocles@gmail.com>
2015-05-11 01:40:03 +0100

    Reset evaluation script

--- src/evaluate.sh
+++ src/evaluate.sh
@@ -1,8 +1,8 @@
 # table headers
-echo "oTime, oBytes, oAlloc, oDealloc, cTime, cBytes, cAlloc, cDealloc,mimics, guards, mimicReadProps, mimicWriteProps, mimicFunCalls, guardReadProps, guardWriteProps, guardFunCalls"; 
+echo "name, oTime, oBytes, oAlloc, oDealloc, cTime, cBytes, cAlloc, cDealloc,mimics, guards, mimicReadProps, mimicWriteProps, mimicFunCalls, guardReadProps, guardWriteProps, guardFunCalls"; 
 
 while read p; do
 	a=($p); 
 	#echo $p;
-	echo $(node profile.js -count ${a[0]}/compiled/${a[1]}) ;
+	echo ${a[1]}, $(node profile.js ${a[0]}/tests/${a[1]}), $(node profile.js ${a[0]}/compiled/${a[1]}), $(node profile.js -count ${a[0]}/compiled/${a[1]}) ;
 done < toProfile.txt
